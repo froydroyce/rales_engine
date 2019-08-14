306b3c092d5080a22cd7a38f3226ecef95894e64
Sejin Kim <froydroyce@gmail.com>
2019-08-14 17:30:39 -0600

    Add functionality to get merchants ranked by items sold

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,12 +2,13 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :transactions, through: :invoices
-  has_many :invoice_items, through: :items
+  # has_many :invoice_items, through: :items
   has_many :invoice_items, through: :invoices
 
   def self.by_total_revenue(limit)
     select("merchants.*, SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue")
-      .joins(:invoice_items, :transactions)
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.successful)
       .group(:id)
       .order("revenue DESC")
       .limit(limit)
@@ -15,7 +16,8 @@ class Merchant < ApplicationRecord
 
   def self.by_items_sold(limit)
     select("merchants.*, SUM(invoice_items.quantity) AS items_sold")
-      .joins(:invoice_items)
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.successful)
       .group(:id)
       .order("items_sold DESC")
       .limit(limit)
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,3 +3,7 @@ class Transaction < ApplicationRecord
 
   scope :successful, -> { where(result: "success") }
 end
+
+# SELECT  merchants.*, SUM(invoice_items.quantity) AS items_sold FROM "merchants" INNER JOIN "invoices" ON "invoices"."merchant_id" = "merchants"."id" INNER JOIN "invoice_items" ON "invoice_items"."invoice_id" = "invoices"."id" INNER JOIN "transactions" ON "transactions"."invoice_id" = "invoices"."id" WHERE "transactions"."result" = 'success' GROUP BY "merchants"."id" ORDER BY items_sold DESC LIMIT 8
+# SELECT  merchants.*, SUM(invoice_items.quantity) AS items_sold FROM "merchants" INNER JOIN "invoices" ON "invoices"."merchant_id" = "merchants"."id" INNER JOIN "transactions" ON "transactions"."invoice_id" = "invoices"."id" INNER JOIN "invoices" "invoices_merchants_join" ON "invoices_merchants_join"."merchant_id" = "merchants"."id" INNER JOIN "invoice_items" ON "invoice_items"."invoice_id" = "invoices_merchants_join"."id" WHERE "transactions"."result" = 'success' GROUP BY "merchants"."id" ORDER BY items_sold DESC LIMIT 8
+# SELECT  merchants.*, SUM(invoice_items.quantity) AS items_sold FROM "merchants" INNER JOIN "invoices" ON "invoices"."merchant_id" = "merchants"."id" INNER JOIN "invoice_items" ON "invoice_items"."invoice_id" = "invoices"."id" INNER JOIN "invoices" "invoices_merchants_join" ON "invoices_merchants_join"."merchant_id" = "merchants"."id" INNER JOIN "transactions" ON "transactions"."invoice_id" = "invoices_merchants_join"."id" WHERE "transactions"."result" = 'success' GROUP BY "merchants"."id" ORDER BY items_sold DESC LIMIT 8
